f5649335872d32df17ab85441cfe164eaadf4587
thukyaw11 <helloeleven968@gmail.com>
2021-01-09 00:26:55 +0630

    build: api route

--- helpers/apis/Api.js
+++ helpers/apis/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 let BaseApi = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  // baseURL: 'http://localhost:8000/api'
+  baseURL: 'http://api.unionpowerpac.com.mm/api'
 })
 
 let Api = function() {
